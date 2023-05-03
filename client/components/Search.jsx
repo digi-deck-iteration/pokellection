ec5bc3416e0b36124035acc30f905a76231bdfd4
Camina Drummer <drummer.camina@pm.me>
2023-05-03 19:28:21 -0400

    Tweaks

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -8,7 +8,7 @@
 import React from "react";
 
 const Search = (props) => {
-  
+  const searchResults = [];
   searchResults.map((card) => {
     <Card cid={card.id} cdate={card.tcgplayer_updated_at} curl={card.tcgplayer_url} cprices={card.tcgplayer_prices} cname={card.name} cimage={card.image_url}/>
   })
