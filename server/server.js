c22a47202182b442cec61c1733f3805018b64ad6
Catherine Kim <ckim722@gmail.com>
2023-05-03 18:58:15 -0400

    changes

--- server/server.js
+++ server/server.js
@@ -1,22 +1,49 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 
+<<<<<<< HEAD
 const signupRouter = require('./routes/signupRouter')
 const pokemonRouter = require('./routes/pokemonRouter')
 const loginRouter = require('./routes/loginRouter')
 const catPokeRouter = require('./routes/catPokeRouter');
 require('dotenv').config()
+=======
+const db = require('./models/db');
+const oAuthSessionModel = require('./models/oAuthSessionModel');
+const signupRouter = require('./routes/signupRouter');
+const pokemonRouter = require('./routes/pokemonRouter');
+const loginRouter = require('./routes/loginRouter');
+const oAuthRouter = require('./routes/oAuthRouter');
+require('dotenv').config();
+>>>>>>> a95ff8353bc3bdedfb18bb98be7e3c291fdf07f2
 // app.use('/', express.static(path.join(__dirname,'')))
 app.use(express.json());
+app.use(cookieParser());
+
+
+//AUTHENTICATION ROUTE
+app.get('/api/isloggedin', (req, res) => {
+  const { cookie } = req.cookies;
+  console.log("COOKIES", cookie)
+  oAuthSessionModel.findOne({ cookieId: cookie }).then((authenticatedUser) => {
+    if (!authenticatedUser) res.status(200).json({ authenticated: false });
+    res.status(200).json({ authenticated: true });
+  });
+});
 
 //ROUTERS
 app.use('/api/signup', signupRouter);
 app.use('/api/pokemon', pokemonRouter);
 app.use('/api/login', loginRouter);
+<<<<<<< HEAD
 app.use('/api/catRouter', catPokeRouter);
 
 
+=======
+app.use('/api/oauth', oAuthRouter);
+>>>>>>> a95ff8353bc3bdedfb18bb98be7e3c291fdf07f2
 
 // CATCH ALL
 app.use('*', (req, res) => {
@@ -34,7 +61,6 @@ app.use((err, req, res, next) => {
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
-
 
 // SERVER
 app.listen(PORT, () => {
